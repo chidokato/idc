0f0e86c2a6c0439d520fbcec92a1fb0318b1ef41
chidokato <tuan.pn92@gmail.com>
2025-12-04 11:50:54 +0700

    update

--- resources/views/account/layout/menu.blade.php
+++ resources/views/account/layout/menu.blade.php
@@ -1,8 +1,8 @@
 <div class="menu-account">
 	<ul>
 		<li><a href="account/task">Quản lý</a></li> |
-		<li><a href="account/mkt-register">Đăng ký</a></li> | 
-		<li><a href="account/task">Đóng tiền</a></li> |
-		<li><a href="account/report">QL Tổng</a></li>
+		<li><a href="account/mkt-register">Đăng ký</a></li> @if(Auth::User()->rank < 3) | 
+		<li><a href="account/task">Đóng tiền</a></li> @endif @if(Auth::User()->rank == 1) |
+		<li><a href="account/report">QL Tổng</a></li>@endif 
 	</ul>
 </div>
