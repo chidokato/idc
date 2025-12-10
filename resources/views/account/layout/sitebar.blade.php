dc70636e66f92c7b44d8fddf075c616d5bf5e206
chidokato <tuan.pn92@gmail.com>
2025-12-10 11:53:41 +0700

    update

--- resources/views/account/layout/sitebar.blade.php
+++ resources/views/account/layout/sitebar.blade.php
@@ -11,7 +11,7 @@
 <div class="row-btn">
     <button class="btn btn-dangky" onclick="window.location.href='{{route('account.mktregister')}}'">ĐĂNG KÝ MARKETING</button>
     <button class="btn btn-quanly" onclick="window.location.href='{{route('task.index')}}'">QUẢN LÝ TÁC VỤ</button>
-    @if(Auth::User()->rank < 3)<button class="btn btn-quanly" onclick="window.location.href='{{route('task.index')}}'">QUẢN LÝ ĐÓNG TIỀN</button>@endif
+    <!-- @if(Auth::User()->rank < 3)<button class="btn btn-quanly" onclick="window.location.href='{{route('task.index')}}'">QUẢN LÝ ĐÓNG TIỀN</button>@endif -->
     @if(Auth::User()->rank == 1)<button class="btn btn-quanly" onclick="window.location.href='{{route('report.index')}}'">QUẢN LÝ TỔNG</button>@endif
 </div>
 
